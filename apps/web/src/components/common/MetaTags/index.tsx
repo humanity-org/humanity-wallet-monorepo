839765f044744a92421db08fa75903ee0da92464
ajimeno04 <jimenobeltran11@gmail.com>
2025-09-30 12:40:01 +0200

    fix: favicon

--- apps/web/src/components/common/MetaTags/index.tsx
+++ apps/web/src/components/common/MetaTags/index.tsx
@@ -39,11 +39,11 @@ const MetaTags = ({ prefetchUrl }: { prefetchUrl: string }) => (
     <link rel="manifest" href="/safe.webmanifest" />
 
     {/* Favicons */}
-    <link rel="shortcut icon" href="/favicons/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000" />
+    <link rel="shortcut icon" href="/images/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
+    <link rel="mask-icon" href="/images/favicons/safari-pinned-tabs.svg" color="#000" />
   </>
 )
 
