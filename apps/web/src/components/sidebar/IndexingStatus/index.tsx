782fd2825a1ccdcf9a8207af23cc158471f58272
JohnPochta <johnpochta1@gmail.com>
2025-10-28 12:15:03 +0100

    feat(status-page): added tx service status page

--- apps/web/src/components/sidebar/IndexingStatus/index.tsx
+++ apps/web/src/components/sidebar/IndexingStatus/index.tsx
@@ -5,8 +5,12 @@ import useAsync from '@safe-global/utils/hooks/useAsync'
 import useChainId from '@/hooks/useChainId'
 import useIntervalCounter from '@/hooks/useIntervalCounter'
 import { OpenInNewRounded } from '@mui/icons-material'
+import { IS_PRODUCTION } from '@/config/constants'
 
-const STATUS_PAGE = 'https://status.safe.global'
+const STATUS_PAGE = IS_PRODUCTION
+  ? 'https://tx-status-app.safe.protofire.io/?serviceUrl=https://gateway.safe.humanity.org'
+  : 'https://tx-status-app.safe.protofire.io/?serviceUrl=https://gateway.staging.safe.humanity.org'
+
 const MAX_SYNC_DELAY = 1000 * 60 * 5 // 5 minutes
 const POLL_INTERVAL = 1000 * 60 // 1 minute
 
